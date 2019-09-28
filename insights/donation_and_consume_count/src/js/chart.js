5b701b02951871aa4b211308131cf37cbb1933b7
Alan-chenhx <hanxuan3@illinois.edu>
2019-09-28 14:09:51 -0500

    RADAR

--- insights/donation_and_consume_count/src/js/chart.js
+++ insights/donation_and_consume_count/src/js/chart.js
@@ -5,19 +5,19 @@ function twenty_five_score(year, data) {
     data: {
       labels: data[year]['UNITID'],
       datasets: [{
-        label: 'Critical Reading',
+        label: 'Consumed',
         backgroundColor: 'rgba(255, 206, 86, 0.2)',
-        data: data[year]['SATVR25'],
+        data: data[year]['Consumed'],
       }, {
-        label: 'Math',
+        label: 'Donated',
         backgroundColor: 'rgba(54, 162, 235, 0.2)',
-        data: data[year]['SATMT25'],
+        data: data[year]['Donated'],
       }]
     },
     options: {
       title: {
         display: true,
-        text: year + ' SAT 25th percentile score'
+        text: year + ' Donated and Consumed'
       },
       maintainAspectRatio: true,
       spanGaps: false,
@@ -38,157 +38,11 @@ function twenty_five_score(year, data) {
   });
 
   return myChart;
-}
-
-function seventy_five_score(year, data) {
-  var ctx = document.getElementById("seventy_five_score").getContext('2d');
-  var myChart = new Chart(ctx, {
-    type: 'radar',
-    data: {
-      labels: data[year]['UNITID'],
-      datasets: [{
-        label: 'Critical Reading',
-        backgroundColor: 'rgba(153, 102, 255, 0.2)',
-        data: data[year]['SATVR75'],
-      }, {
-        label: 'Math',
-        backgroundColor: 'rgba(255, 99, 132, 0.2)',
-        data: data[year]['SATMT75'],
-      }]
-    },
-    options: {
-      title: {
-        display: true,
-        text: year + ' SAT 75th percentile score'
-      },
-      maintainAspectRatio: true,
-      spanGaps: false,
-      elements: {
-        line: {
-          tension: 0.000001
-        }
-      },
-      plugins: {
-        filler: {
-          propagate: false
-        },
-        'samples-filler-analyser': {
-          target: 'chart-analyser'
-        }
-      }
-    }
-  });
-
-  return myChart;
-}
-
-function twenty_five_score_act(year, data) {
-  data = delete_act_empty(year, data);
-  var ctx = document.getElementById("twenty_five_score_act").getContext('2d');
-  var myChart = new Chart(ctx, {
-    type: 'radar',
-    data: {
-      labels: data[year]['UNITID'],
-      datasets: [{
-        label: 'English',
-        backgroundColor: 'rgba(153, 102, 255, 0.2)',
-        data: data[year]['ACTEN25'],
-      }, {
-        label: 'Math',
-        backgroundColor: 'rgba(255, 99, 132, 0.2)',
-        data: data[year]['ACTMT25'],
-      }]
-    },
-    options: {
-      title: {
-        display: true,
-        text: year + ' ACT 25th percentile score'
-      },
-      maintainAspectRatio: true,
-      spanGaps: false,
-      elements: {
-        line: {
-          tension: 0.000001
-        }
-      },
-      plugins: {
-        filler: {
-          propagate: false
-        },
-        'samples-filler-analyser': {
-          target: 'chart-analyser'
-        }
-      }
-    }
-  });
-
-  return myChart;
-}
-
-function seventy_five_score_act(year, data) {
-  data = delete_act_empty(year, data);
-  var ctx = document.getElementById("seventy_five_score_act").getContext('2d');
-  var myChart = new Chart(ctx, {
-    type: 'radar',
-    data: {
-      labels: data[year]['UNITID'],
-      datasets: [{
-        label: 'English',
-        backgroundColor: 'rgba(75, 192, 192, 0.2)',
-        data: data[year]['ACTEN75'],
-      }, {
-        label: 'Math',
-        backgroundColor: 'rgba(255, 159, 64, 0.2)',
-        data: data[year]['ACTMT75'],
-      }]
-    },
-    options: {
-      title: {
-        display: true,
-        text: year + ' ACT 75th percentile score'
-      },
-      maintainAspectRatio: true,
-      spanGaps: false,
-      elements: {
-        line: {
-          tension: 0.000001
-        }
-      },
-      plugins: {
-        filler: {
-          propagate: false
-        },
-        'samples-filler-analyser': {
-          target: 'chart-analyser'
-        }
-      }
-    }
-  });
-
-  return myChart;
-}
-
-function delete_act_empty(year, data) {
-  for (var i = 0; i < data[year]['ACTMT25'].length; i++) {
-    if (data[year]['ACTMT25'][i] === '' || data[year]['ACTEN25'][i] === '') {
-      console.log(1);
-      data[year]['UNITID'].splice(i, 1);
-      data[year]['ACTMT25'].splice(i, 1);
-      data[year]['ACTMT75'].splice(i, 1);
-      data[year]['ACTEN25'].splice(i, 1);
-      data[year]['ACTEN75'].splice(i, 1);
-      i--;
-    }
-  }
-  return data;
 }
 
 function update(year, data) {
   return [
     twenty_five_score(year, data),
-    seventy_five_score(year, data),
-    twenty_five_score_act(year, data),
-    seventy_five_score_act(year, data)
   ];
 }
 
